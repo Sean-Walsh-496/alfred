0fa0411ebe19763d1344653185b10061f734ecdc
Sean <sean@voltagency.co>
2022-01-22 15:18:18 -0600

    filled screen and tested

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,30 @@
 <template>
-  <div>
+  <div id="main">
+    <TaskBar></TaskBar>
+    <div style="background: black; height: 100%; width: 100%;">
 
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import TaskBar from "../components/taskbar.vue";
 
 export default {
   name: 'Home',
   components: {
-
+    TaskBar
   }
 }
 </script>
+
+<style scoped>
+  #main{
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: row;
+  }
+</style>
