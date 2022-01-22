e7ab7ff964edc0cb2614a899effbe6e8650f1cc1
Sean <sean@voltagency.co>
2022-01-22 15:42:27 -0600

    basic outline

---
+++ src/components/dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="main"></div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    #main{
+        background: var(--background-1);
+        height: 100%;
+        width: 100%;
+    }
+</style>
