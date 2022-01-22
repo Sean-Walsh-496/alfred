1469dfe51edf6fa6a0a62aedb40821e2db0dca80
Sean <sean@voltagency.co>
2022-01-22 15:18:03 -0600

    adjusted the taskbar visuals

---
+++ src/components/taskbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="bar">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TaskBar",
+
+}
+</script>
+
+<style scoped>
+    #bar {
+        height: 100%;
+        width: 20%;
+        max-width: 100px;
+        background: var(--background-1);
+        border-right: 2px solid var(--border);  
+    }
+</style>
