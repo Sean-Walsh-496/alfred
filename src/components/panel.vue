f7bed6c57c44c42a04c34cbdee049f13a836594b
Sean <sean@voltagency.co>
2022-01-22 15:52:21 -0600

    outlined

---
+++ src/components/panel.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="main">
+        <div id="top-bar">
+
+        </div>
+        <div>
+            <slot></slot>
+        </div>
+        <div>
+
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "Panel"
+}
+</script>
+
+<style scoped>
+
+</style>
