de01ff90b3a187635e4e8cd002cb70b826287ac1
Sean <sean@voltagency.co>
2022-01-24 21:06:32 -0600

    clicks off the target

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ createApp(App).use(store).use(router).mount('#app')
 
 addEventListener("mouseup", (e) => {
     store.state.mouse.down = false;
+    store.state.mouse.target = null;
 });
 
 addEventListener("mousedown", (e) => {
