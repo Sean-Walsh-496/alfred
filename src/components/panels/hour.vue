bf6a0cd1bc56b9835ef6e6a2597d50d5edbe4491
Sean <sean@voltagency.co>
2022-02-03 15:35:33 -0600

    creates the hour component

---
+++ src/components/panels/hour.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="content">
+        {{id}}
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: "Hour",
+    props: ["parent", "id"],
+
+}
+</script>
+
+<style scoped>
+    .content{
+        width: 100%;
+        height: 100%;
+        background: chartreuse;
+    }
+
+</style>
