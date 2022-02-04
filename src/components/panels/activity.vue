c6d9b61742a19c60bbd5624a2abc4d2d8f8dcce8
Sean <sean@voltagency.co>
2022-02-04 15:50:20 -0600

    basic outline of the activity vue

---
+++ src/components/panels/activity.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="content">
+
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: "Activity",
+    props: ["day"]
+
+}
+</script>
+
+<style scoped>
+
+    .content{
+        position: absolute;
+
+    }
+
+</style>
