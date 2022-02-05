b1a7ffa4ad61a7b239e79e1c82f743b2946e5729
Sean <sean@voltagency.co>
2022-02-05 15:46:56 -0600

    now can track with its window

--- src/components/panels/activity.vue
+++ src/components/panels/activity.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="content">
+    <div class="content" :style="{top: rect.y + 'px', left: rect.x + 'px', width: (rect.right - rect.x) + 'px', height: (rect.bottom - rect.y) + 'px'}">
 
     </div>
     
@@ -8,7 +8,33 @@
 <script>
 export default {
     name: "Activity",
-    props: ["day"]
+    props: ["day", "parent"],
+    data(){
+        let panel = this.$store.state.homePage.dashboard.panels[this.day.parent.id];
+        let data = panel.content.hours[this.parent.id - 1];
+        let rect = this.parent.$el.getBoundingClientRect()
+        return {
+            data: data,
+            rect: rect
+
+        };
+    },
+    methods: {
+    },
+    computed: {
+        panelData(){
+            return this.$store.state.homePage.dashboard.panels[this.day.parent.id];
+        }
+    },
+    watch: {
+        panelData: {
+            handler(){
+                this.rect = this.parent.$el.getBoundingClientRect();
+            },
+            deep: true
+        }
+    },
+
 
 }
 </script>
@@ -17,6 +43,9 @@ export default {
 
     .content{
         position: absolute;
+        height: 100px;
+        width: 100px;
+        background: blue;
 
     }
 
