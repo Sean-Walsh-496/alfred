a7163250c2299ffe7a14a5103a8c325de3dde5ec
Sean <sean@voltagency.co>
2022-02-14 16:38:43 -0600

    new colors, incorporating a dark theme as well

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,22 @@
 
 <style>
 :root {
-  --background-1: #2e3440;
-  --border: #d5deeb;
-  --red: #c06069;
-  --blue: #89b9c7;
-  --green: #a3bd8d;
-  --yellow: #eacb89;
-  --purple: #b28fad;
-  --white: #d5deeb;
+  --background-1: #eeeff2;
+  --border: #a8a8a8;
+  --red: #FF595E;
+  --blue: #1982C4;
+  --green: #8AC926;
+  --yellow: #FFCA3A;
+  --purple: #6A4C93;
+  --white: #ffffff;
+  --d-background-1: #2e3440;
+  --d-border: #d5deeb;
+  --d-red: #c06069;
+  --d-blue: #89b9c7;
+  --d-green: #a3bd8d;
+  --d-yellow: #eacb89;
+  --d-purple: #b28fad;
+  --d-white: #d5deeb;
 }
 
 
